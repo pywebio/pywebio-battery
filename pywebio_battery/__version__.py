f30d345999301f812f70b219b50e4cc40b42d689
wangweimin <wang0.618@qq.com>
2022-12-13 20:28:18 +0800

    release v0.4.0

--- pywebio_battery/__version__.py
+++ pywebio_battery/__version__.py
@@ -1,8 +1,8 @@
 __package__ = 'pywebio_battery'
 __description__ = 'Utilities that help write PyWebIO apps quickly and easily.'
 __url__ = 'https://pywebio.readthedocs.io/en/latest/battery.html'
-__version__ = "0.3.0"
-__version_info__ = (0, 3, 0, 0)
+__version__ = "0.4.0"
+__version_info__ = (0, 4, 0, 0)
 __author__ = 'WeiminWang'
 __author_email__ = 'wang0.618@qq.com'
 __license__ = 'MIT'
